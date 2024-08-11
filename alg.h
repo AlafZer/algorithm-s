cb25c641d32a9aeab8947ed03343c79ebbc3d95e
AlafZer <123867755+AlafZer@users.noreply.github.com>
2024-08-11 15:00:49 +1000

    Update alg.h

--- alg.h
+++ alg.h
@@ -1,9 +1,8 @@
 #pragma once
 #include <vector>
-#include <iostream>
 
 namespace alg
 {
-	void qsort(std::vector<int>& vec);
-	int binSch(std::vector<int>& vec, int item);
+	void qsort(std::vector<int>&);
+	int binSch(std::vector<int>&, int);
 }
